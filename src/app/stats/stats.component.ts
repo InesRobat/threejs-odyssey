aaadd12f17b8617492b98d2d3fd9def4c02b75aa
InesRobat <inesrobat@outlook.com>
2024-09-20 12:01:39 +0200

    :art:

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,20 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, QueryList, Renderer2, ViewChildren } from '@angular/core';
+import { interval } from 'rxjs';
+import { switchMap, take } from 'rxjs/operators';
+import { NumberFlipDirective } from '../directives/number-flip.directive';
 
 @Component({
   selector: 'app-stats',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
   templateUrl: './stats.component.html',
-  styleUrl: './stats.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./stats.component.scss'],
+  standalone: true,
+  imports: [CommonModule, NumberFlipDirective],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatsComponent implements AfterViewInit {
-
-  @ViewChildren('statsNumbers') statsNumbers!: QueryList<ElementRef<HTMLDivElement>>;
-
+  @ViewChildren('scrollNum') scrollNum!: QueryList<ElementRef<HTMLSpanElement>>;
   items = [
     { text: 'Chapters', number: 7 },
     { text: 'Lessons', number: 66 },
@@ -22,5 +21,52 @@ export class StatsComponent implements AfterViewInit {
     { text: 'Challenging', number: 100 },
   ];
 
-  ngAfterViewInit(): void { }
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    this.updateView();
+  }
+
+  updateView(): void {
+    this.items.forEach((item) => this.setup(item.number));
+  }
+
+  setup(startNum: number): void {
+    const digits = startNum.toString().split('');
+
+    this.createSpans(digits);
+    this.scrollNumber(digits);
+  }
+
+  private createSpans(digits: string[]): void {
+    this.scrollNum.forEach((num, index) => {
+      const stat = num.nativeElement as HTMLSpanElement;
+      digits.forEach((digit) => {
+        const span = this.renderer.createElement('span');
+        this.renderer.setStyle(span, 'transform', 'translateY(-1000%)');
+        this.renderer.setAttribute(span, 'data-value', digit);
+        this.renderer.appendChild(stat, span);
+      });
+    });
+  }
+
+  scrollNumber(digits: string[]): void {
+    this.scrollNum.forEach((num, index) => {
+      const stat = num.nativeElement as HTMLSpanElement;
+      const ticks = stat.querySelectorAll('span[data-value]');
+      ticks.forEach((tick: any, i) => {
+        tick.style.transform = `translateY(-${100 * parseInt(digits[index], 10)}%)`;
+      });
+      setTimeout(() => {
+        stat.style.width = `${digits.length * 28}px`;
+      }, 100);
+      interval(60).pipe(take(digits.length * 1000), switchMap(() => interval(50))).subscribe(() => {
+        ticks.forEach((tick: any, i) => {
+          const value = parseInt(digits[index], 10) - i;
+          tick.style.transform = `translateY(-${value}%)`;
+        });
+      });
+    });
+  }
 }
+
